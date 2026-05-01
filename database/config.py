035cf6d3ae4d508ad78be04a864295226391b880
Sudeguslu <Sude200270@gmail.com>
2026-05-01 15:50:23 +0300

    created a new database and connected

--- database/config.py
+++ database/config.py
@@ -3,6 +3,9 @@
 # MONGO_URI satırına kendi connection string'ini yapıştır.
 # ─────────────────────────────────────────────────────────────
 
-MONGO_URI = "mongodb+srv://celal:123@emotion-recognition-db.4cwacxf.mongodb.net/?appName=Emotion-Recognition-DB" 
+# MONGO_URI = "mongodb+srv://celal:123@emotion-recognition-db.4cwacxf.mongodb.net/?appName=Emotion-Recognition-DB" 
 
-DB_NAME   = "Emotion-Recognition-DB"
+# DB_NAME   = "Emotion-Recognition-DB"
+
+MONGO_URI = "mongodb+srv://sude:sude123@cluster0.a4cmn9d.mongodb.net/?appName=Cluster0"
+DB_NAME = "Emotion_Recognition_DB"
